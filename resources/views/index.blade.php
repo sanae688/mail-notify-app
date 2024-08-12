1235638c41bebebac730d1ac940ead46caf4004f
sanae688 <music.37ts@gmail.com>
2024-08-12 13:04:47 +0900

    #0001 Laravel Sail環境構築

---
+++ resources/views/index.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>mail-notify-app</title>
+
+    @viteReactRefresh
+    @vite(['resources/sass/app.scss', 'resources/ts/index.tsx'])
+
+</head>
+
+<body class="antialiased">
+    <div id="app"></div>
+</body>
+
+</html>
